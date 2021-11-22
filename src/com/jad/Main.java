8610fe22969ac03fc74377b2b57e92c56800fcbe
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2021-11-22 22:10:45 +0100

    Just the Main class

--- src/com/jad/Main.java
+++ src/com/jad/Main.java
@@ -8,37 +8,31 @@ public class Main {
     public static void main(String[] args) {
         RotatingBuffer<Integer> buffer = new RotatingBuffer<Integer>(Integer.class, 2);
         Random rand = new Random();
-        Thread writer = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 100; i++) {
-                    if(!buffer.write(i)) {
-                        System.out.print("-");
-                    }
-                    try {
-                        Thread.sleep(rand.nextInt(500));
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        Thread writer = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                if(!buffer.write(i)) {
+                    System.out.print("-");
+                }
+                try {
+                    Thread.sleep(rand.nextInt(500));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         });
 
-        Thread reader = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (; ; ) {
-                    Integer temp = buffer.read();
-                    if (temp != null) {
-                        System.out.print("[" + temp + "]");
-                    } else {
-                        System.out.print(".");
-                    }
-                    try {
-                        Thread.sleep(rand.nextInt(500));
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        Thread reader = new Thread(() -> {
+            for (; ; ) {
+                Integer temp = buffer.read();
+                if (temp != null) {
+                    System.out.print("[" + temp + "]");
+                } else {
+                    System.out.print(".");
+                }
+                try {
+                    Thread.sleep(rand.nextInt(500));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         });
--- src/com/jad/RotatingBuffer/RotatingBufferWriter.java
+++ src/com/jad/RotatingBuffer/RotatingBufferWriter.java
@@ -1,18 +1,13 @@
 package com.jad.RotatingBuffer;
 
 class RotatingBufferWriter<Data> extends RotatingBufferActor<Data> implements IRotatingBufferWriter<Data> {
-    public RotatingBufferWriter(final RotatingBuffer<Data> buffer) {
-        super(buffer);
+    @Override
+    public boolean write(final Data data) {
+        return false;
     }
 
     @Override
-    public boolean write(final Data data) {
-        if ((this.getBuffer().getReader().getIndex() != this.getIndex()) || this.getBuffer().isEmpty()) {
-            this.getBuffer().getDataArray()[this.getIndex()] = data;
-            this.getBuffer().setEmpty(false);
-            this.incrementIndex();
-            return true;
-        }
-        return false;
+    public int getIndex() {
+        return 0;
     }
 }
